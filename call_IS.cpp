21d4fc0faf0b563da6910bec8f633324efac2a9e
PranavMenon25 <144007090+PranavMenon25@users.noreply.github.com>
2024-03-04 23:56:19 +0530

    Pranav
    Updated to check the type of instruction and call the corresponding instruction type.

--- call_IS.cpp
+++ call_IS.cpp
@@ -1,20 +1,32 @@
 //commands to compile this file 
-//  g++ -o my_program InstructionSet.cpp call_IS.cpp
-//  ./my_program
+//  g++ call_IS.cpp
+//  ./axe
 
 #include <iostream>
-#include "InstructionSet.h"
+#include "InstructionSet.cpp"
 using namespace std;
 
 
 int main() {
     InstructionSet instructionSet;
     RFormatInstruction rFormatInstruction;
-
+    IInstructionFormat iFormatinstruction;
     // Example usage
-    string instructionLine = "add x5,x6,x9";
-    string machineCode = rFormatInstruction.generateMachineCode(instructionLine);
-    cout << "Machine Code: " << machineCode << endl;
+    string instructionLine = "jalr  x13  ,  324 (x16)";
+
+    cout<<"Instruction = "<<instructionLine<<endl;
+    string format = instructionSet.getFormat(instructionLine);
+    cout<<"FORMAT = "<<format<<endl;
+
+    if(format == "R"){
+        string machineCode = rFormatInstruction.generateMachineCode(instructionLine);
+        cout << "Machine Code: " << machineCode << endl;
+    }
+    
+    else if(format == "I"){
+        string machineCode = iFormatinstruction.generateMachineCode(instructionLine);
+        cout << "Machine Code: " << machineCode << endl;
+    }
 
     return 0;
 }
