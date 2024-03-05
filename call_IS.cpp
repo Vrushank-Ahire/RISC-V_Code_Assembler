dc6aac64e330a5c64c3e0a57dab8d3acd7f4409a
PranavMenon25 <144007090+PranavMenon25@users.noreply.github.com>
2024-03-05 16:41:48 +0530

    S added

--- call_IS.cpp
+++ call_IS.cpp
@@ -1,6 +1,6 @@
 //commands to compile this file 
-//  g++ call_IS.cpp
-//  ./axe
+//  g++ -o my_program InstructionSet.cpp call_IS.cpp
+//  ./my_program
 
 #include <iostream>
 #include "InstructionSet.cpp"
@@ -11,8 +11,9 @@ int main() {
     InstructionSet instructionSet;
     RFormatInstruction rFormatInstruction;
     IInstructionFormat iFormatinstruction;
+    SInstruction sFormatInstruction;
     // Example usage
-    string instructionLine = "jalr  x13  ,  324 (x16)";
+    string instructionLine = "lb x14, 43(x15)";
 
     cout<<"Instruction = "<<instructionLine<<endl;
     string format = instructionSet.getFormat(instructionLine);
@@ -26,6 +27,11 @@ int main() {
     else if(format == "I"){
         string machineCode = iFormatinstruction.generateMachineCode(instructionLine);
         cout << "Machine Code: " << machineCode << endl;
+    }
+
+    else if(format == "S"){
+        string machinecode = sFormatInstruction.generateMachineCode(instructionLine);
+        cout<< "Machine Code: "<<machinecode<<endl;
     }
 
     return 0;
