2cbc2054f2af6eeb46f7156f876530ef61d2c195
PranavMenon25 <144007090+PranavMenon25@users.noreply.github.com>
2024-03-07 17:57:25 +0530

    Update call_IS.cpp
    Made the class

--- call_IS.cpp
+++ call_IS.cpp
@@ -5,35 +5,56 @@
 #include <iostream>
 #include "InstructionSet.cpp"
 using namespace std;
+#include "output.mc"
 
+int program_counter;
 
-int main() {
+void program_zero(){
+    program_counter = 0;
+}
+
+string final_machinecode(string line) {
+    //cout<<line<<endl;
+    if(line[0] == '.'){
+        cout<<"Directive"<<endl;
+        return "" ;
+    }
+    cout<<"*";
     InstructionSet instructionSet;
     RFormatInstruction rFormatInstruction;
     IInstructionFormat iFormatinstruction;
     SInstruction sFormatInstruction;
     // Example usage
-    string instructionLine = "lb x14, 43(x15)";
-
+    string instructionLine = "add x14,x15,x17";
+    instructionLine = line;
     cout<<"Instruction = "<<instructionLine<<endl;
     string format = instructionSet.getFormat(instructionLine);
     cout<<"FORMAT = "<<format<<endl;
+    string machineCode;
 
     if(format == "R"){
-        string machineCode = rFormatInstruction.generateMachineCode(instructionLine);
-        cout << "Machine Code: " << machineCode << endl;
+        program_counter += 4;
+        machineCode = rFormatInstruction.generateMachineCode(instructionLine);
+        cout << "0x"<<program_counter<<"Machine Code: " << machineCode << endl;
     }
     
     else if(format == "I"){
-        string machineCode = iFormatinstruction.generateMachineCode(instructionLine);
-        cout << "Machine Code: " << machineCode << endl;
+        program_counter += 4;
+
+        machineCode = iFormatinstruction.generateMachineCode(instructionLine);
+        cout << "0x"<<program_counter<<"Machine Code: " << machineCode << endl;
     }
 
     else if(format == "S"){
-        string machinecode = sFormatInstruction.generateMachineCode(instructionLine);
-        cout<< "Machine Code: "<<machinecode<<endl;
-    }
+        program_counter += 4;
 
-    return 0;
+        machineCode = sFormatInstruction.generateMachineCode(instructionLine);
+        cout<< "0x"<<program_counter<<"Machine Code: "<<machineCode<<endl;
+    }
+    
+  //  "output.mc" << machineCode << endl;
+
+    
+    return "";
 }
 
