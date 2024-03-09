f964ac54267ef5437d5dc1e23eff5104508e2ca3
PranavMenon25 <144007090+PranavMenon25@users.noreply.github.com>
2024-03-09 19:41:44 +0530

    Update call_IS.cpp

--- call_IS.cpp
+++ call_IS.cpp
@@ -1,6 +1,6 @@
-//commands to compile this file 
-//  g++ -o my_program InstructionSet.cpp call_IS.cpp
-//  ./my_program
+// commands to compile this file
+//   g++ -o my_program InstructionSet.cpp call_IS.cpp
+//   ./my_program
 
 #include <iostream>
 #include "InstructionSet.cpp"
@@ -9,52 +9,150 @@ using namespace std;
 
 int program_counter;
 
-void program_zero(){
+void program_zero()
+{
     program_counter = 0;
 }
 
-string final_machinecode(string line) {
-    //cout<<line<<endl;
-    if(line[0] == '.'){
-        cout<<"Directive"<<endl;
-        return "" ;
+string program_hex()
+{
+    
+    int temp = program_counter;
+    string final = "";
+    while (temp > 0)
+    {
+        int mod = temp % 16;
+        if (mod <= 9)
+        {
+            final = (char)(mod + '0') + final;
+        }
+
+        else
+        {
+            final = (char)('A' + mod - 10) + final;
+        }
+
+        temp /= 16;
     }
-    cout<<"*";
+    if (final.length() == 0)
+    {
+        return "0";
+    }
+    return final;
+}
+
+string to_hex(string input)
+{
+    string final;
+    // outputFile<<input<<endl;
+    for (int i = 0; i < 32; i++)
+    {
+        int temp = 0;
+        for (int j = 0; j < 4; j++)
+        {
+            //  outputFile<<i+j<<endl;
+            temp *= 2;
+            temp += input[i + j] - '0';
+            // outputFile<<temp<<endl;
+        }
+
+        // outputFile<<temp<<endl;
+
+        if (temp <= 9)
+        {
+            char k = temp + '0';
+            final += k;
+        }
+
+        else
+        {
+            final += (char)('A' + temp - 10);
+        }
+
+        i = i + 3;
+    }
+
+    return final;
+}
+
+int get_program()
+{
+    return program_counter;
+}
+
+string final_machinecode(string line)
+{
+    // outputFile<<line<<endl;
+    ofstream outputFile("outputDataSeg.txt", std::ios::app);
+    if (line[0] == '.')
+    {
+        return "";
+    }
+
     InstructionSet instructionSet;
     RFormatInstruction rFormatInstruction;
     IInstructionFormat iFormatinstruction;
     SInstruction sFormatInstruction;
-    // Example usage
-    string instructionLine = "add x14,x15,x17";
-    instructionLine = line;
-    cout<<"Instruction = "<<instructionLine<<endl;
+    SBInstruction sbinstruction;
+    UInstruction uinstruction;
+    UJInstruction ujinstruction;
+
+    string instructionLine = line;
+
+    // outputFile<<"Instruction = "<<instructionLine<<endl;
     string format = instructionSet.getFormat(instructionLine);
-    cout<<"FORMAT = "<<format<<endl;
+
+    // outputFile<<"FORMAT = "<<format<<endl;
     string machineCode;
-
-    if(format == "R"){
-        program_counter += 4;
+    // outputFile<<instructionLine;
+    if (format == "R")
+    {
         machineCode = rFormatInstruction.generateMachineCode(instructionLine);
-        cout << "0x"<<program_counter<<"Machine Code: " << machineCode << endl;
-    }
-    
-    else if(format == "I"){
+        // outputFile << "0x"<<program_counter<<"Machine Code: " << machineCode << endl;
+        outputFile << "0x" << program_hex() << " " << to_hex(machineCode) << endl;
         program_counter += 4;
+    }
 
+    else if (format == "I")
+    {
         machineCode = iFormatinstruction.generateMachineCode(instructionLine);
-        cout << "0x"<<program_counter<<"Machine Code: " << machineCode << endl;
-    }
-
-    else if(format == "S"){
+        // outputFile << "0x"<<program_counter<<"Machine Code: " << machineCode << endl;
+        outputFile << "0x" << program_hex() << " " << to_hex(machineCode) << endl;
         program_counter += 4;
-
-        machineCode = sFormatInstruction.generateMachineCode(instructionLine);
-        cout<< "0x"<<program_counter<<"Machine Code: "<<machineCode<<endl;
     }
-    
-  //  "output.mc" << machineCode << endl;
 
-    
+    else if (format == "S")
+    {
+        machineCode = sFormatInstruction.generateMachineCode(instructionLine);
+        // outputFile<< "0x"<<program_counter<<"Machine Code: "<<machineCode<<endl;
+        outputFile << "0x" << program_hex() << " " << to_hex(machineCode) << endl;
+        program_counter += 4;
+    }
+
+    else if (format == "SB")
+    {
+        machineCode = sbinstruction.generateMachineCode(instructionLine, program_counter);
+        // outputFile<<"0x"<<program_counter<<"Machine Code: "<<machineCode<<endl;
+
+        outputFile << "0x" << program_hex() << " " << to_hex(machineCode) << endl;
+        program_counter += 4;
+    }
+
+    else if (format == "U")
+    {
+        machineCode = uinstruction.generateMachineCode(instructionLine);
+        outputFile << "0x" << program_hex() << " " << to_hex(machineCode) << endl;
+        program_counter += 4;
+    }
+
+    else if (format == "UJ")
+    {
+        machineCode = ujinstruction.generateMachineCode(instructionLine, program_counter);
+
+        outputFile << "0x" << program_hex() << " " << to_hex(machineCode) << endl;
+        program_counter += 4;
+    }
+    //  "output.mc" << machineCode << endl;
+
     return "";
 }
-
